c9ee04d994439370a55f82807e8f396b9aa687d2
tuohaidong <1720422565@qq.com>
2024-10-16 22:22:56 +0800

    V1.0.0

---
+++ client/main/process_client2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include "prod_common_ipc_easylogger.h"
+
+int main(void)
+{
+    int count=0;
+    char log[100] = {};
+    while (1)
+    {
+        count++;
+        memset(log, '\0', 100);
+        snprintf(log, 100, "%s:%d", "client2", count);
+        log_a(log);
+        sleep(1);
+        log_f(log);
+        sleep(1);
+        log_e(log);
+        sleep(1);
+        log_w(log);
+        sleep(1);
+        log_i(log);
+        sleep(1);
+        log_d(log);
+        sleep(1);
+    }
+    return 0;
+}
